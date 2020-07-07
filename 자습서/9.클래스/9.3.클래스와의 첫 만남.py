cb94be7e658ee451b24ae9b5c6d7ddc1aed67add
n3nn3 <whi0331@hanmail.net>
2020-07-07 23:03:20 +0900

    0707 c0mmit

---
+++ 자습서/9.클래스/9.3.클래스와의 첫 만남.py
@@ -0,0 +1,26 @@
+# 9.3.1. 클래스 정의 문법
+# 정의 또는 선언
+# 함수선언과 클래스 선언
+# 선언은 재사용하기 위해서
+
+# class 클래스이름
+# ~~~~~~~~
+
+# 타입__클래스__
+
+# 9.3.2. 클래스 객체
+
+# 어트리뷰트와 메서드
+class MyClass:
+    """A simple example class"""
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+
+
+x = MyClass()  # 인스턴스
+
+
+def __init__(self):
+    self.data = []
